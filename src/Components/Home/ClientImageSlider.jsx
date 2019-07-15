b6465294fd71e9fb808368c5eb21849278ebf2bf
GRASSHOPPER <grasshopper@GRASSHOPPERs-iMac.local>
2019-07-15 17:09:35 +0530

    implemented image slider

--- src/Components/Home/ClientImageSlider.jsx
+++ src/Components/Home/ClientImageSlider.jsx
@@ -1,13 +1,104 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { client_images_data2 } from './home_page_data';
 
-export default function ClientImageSlider({ client_images_data, slide_num, total_images }) {
-	return client_images_data[slide_num].map((imgs, index) => (
-		<div className="client_images" key={index}>
-			<img
-				src={process.env.PUBLIC_URL + '/assets/images/client_logo/' + imgs.imgname}
-				alt={imgs.alt}
-				className="img-fit-with-height"
-			/>
-		</div>
-	));
+class ClientImageSlider extends Component {
+	state = {
+		one: 0,
+		two: 1,
+		three: 2,
+		four: 3,
+		total_imgs: client_images_data2.length,
+		time: 0
+	};
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			let choice = Math.floor(Math.random() * 4),
+				random_img = Math.floor(Math.random() * this.state.total_imgs);
+
+			if (
+				random_img !== this.state.one &&
+				random_img !== this.state.two &&
+				random_img !== this.state.three &&
+				random_img !== this.state.four
+			) {
+				switch (choice) {
+					case 0:
+						this.setState({ one: random_img });
+						break;
+					case 1:
+						this.setState({ two: random_img });
+						break;
+					case 2:
+						this.setState({ three: random_img });
+						break;
+					case 3:
+						this.setState({ four: random_img });
+						break;
+
+					default:
+						break;
+				}
+			}
+		}, 3000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<div className="client_images">
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							'/assets/images/client_logo/' +
+							client_images_data2[this.state.one].imgname
+						}
+						alt={client_images_data2[this.state.one].alt}
+						className="img-fit-with-height"
+					/>
+				</div>
+
+				<div className="client_images">
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							'/assets/images/client_logo/' +
+							client_images_data2[this.state.two].imgname
+						}
+						alt={client_images_data2[this.state.two].alt}
+						className="img-fit-with-height"
+					/>
+				</div>
+
+				<div className="client_images">
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							'/assets/images/client_logo/' +
+							client_images_data2[this.state.three].imgname
+						}
+						alt={client_images_data2[this.state.three].alt}
+						className="img-fit-with-height"
+					/>
+				</div>
+
+				<div className="client_images">
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							'/assets/images/client_logo/' +
+							client_images_data2[this.state.four].imgname
+						}
+						alt={client_images_data2[this.state.four].alt}
+						className="img-fit-with-height"
+					/>
+				</div>
+			</Fragment>
+		);
+	}
 }
+
+export default ClientImageSlider;
--- src/Components/Home/home_page_data.js
+++ src/Components/Home/home_page_data.js
@@ -137,6 +137,129 @@ export const client_images_data = {
 	]
 };
 
+export const client_images_data2 = [
+	{
+		imgname: '1.png',
+		alt: 'Sangeetha'
+	},
+	{
+		imgname: '2.png',
+		alt: 'Nagarjuna Ayurveda'
+	},
+	{
+		imgname: '4.png',
+		alt: 'Kallada Software Service'
+	},
+	{
+		imgname: '3.png',
+		alt: 'Praneetha'
+	},
+	{
+		imgname: '5.png',
+		alt: 'Sajaas Design Boutique'
+	},
+	{
+		imgname: '6.png',
+		alt: 'Galeford'
+	},
+	{
+		imgname: '7.png',
+		alt: 'Hindmade'
+	},
+	{
+		imgname: '8.png',
+		alt: 'Ingredients Bazar'
+	},
+	{
+		imgname: '9.png',
+		alt: 'Sundaratheeram Senior Living'
+	},
+	{
+		imgname: '10.png',
+		alt: 'Force India Pharma'
+	},
+	{
+		imgname: '11.png',
+		alt: 'Star360'
+	},
+	{
+		imgname: '12.png',
+		alt: 'Sagara'
+	},
+	{
+		imgname: '13.png',
+		alt: 'Jack & Julie'
+	},
+	{
+		imgname: '14.png',
+		alt: 'Hind Farms'
+	},
+	{
+		imgname: '15.png',
+		alt: 'Sunaja Contracts'
+	},
+	{
+		imgname: '16.png',
+		alt: 'DrLeo Care Redefined'
+	},
+	{
+		imgname: '17.png',
+		alt: 'Makeover'
+	},
+	{
+		imgname: '18.png',
+		alt: 'Dine'
+	},
+	{
+		imgname: '19.png',
+		alt: 'Panigh'
+	},
+	{
+		imgname: '20.png',
+		alt: 'Amrozia'
+	},
+	{
+		imgname: '21.png',
+		alt: 'Gems B School'
+	},
+	{
+		imgname: '22.png',
+		alt: 'Door Windows'
+	},
+	{
+		imgname: '23.png',
+		alt: 'Skill Drive'
+	},
+	{
+		imgname: '24.png',
+		alt: 'CarterX'
+	},
+	{
+		imgname: '25.png',
+		alt: 'Kokarako Restaurant'
+	},
+	{
+		imgname: '26.png',
+		alt: 'LaLucido'
+	},
+	{
+		imgname: '27.png',
+		alt: 'Artoholic'
+	},
+	{
+		imgname: '28.png',
+		alt: 'Gov. Arts & Scince College Uduma'
+	},
+	{
+		imgname: '29.png',
+		alt: 'Sunergies'
+	},
+	{
+		imgname: '30.png',
+		alt: 'Briyas'
+	}
+];
+
 export const best_at_data = [
 	{
 		img: 'd_v_3.png',
